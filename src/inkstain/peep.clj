772074dac11d14d579435e0c463db41d6348fca9
Folcon <folcon@gmail.com>
2026-03-17 17:01:36 +0000

    Update peep

--- src/inkstain/peep.clj
+++ src/inkstain/peep.clj
@@ -9,6 +9,15 @@
    :speed 4.0            ;; tiles per second
    :hp 100
    :max-hp 100
+
+   :attack-range 1.5    ;; melee range in tiles
+   :attack-damage 10    ;; per hit
+   :attack-cooldown 0   ;; counts down
+   :attack-speed 0.5    ;; seconds between attacks
+
+   :hit-timer 0
+   :death-timer 0
+
    :faction faction      ;; :player :ally :enemy
    :state :idle          ;; :idle :moving :attacking
    :path []              ;; waypoints from pathfinding
