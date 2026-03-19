db1e38cf7f8789c4ffb70b59c3e38dd8a6eefd52
Folcon <folcon@gmail.com>
2026-03-19 17:45:18 +0000

    Fixed terminate not moving

--- src/inkstain/fns.clj
+++ src/inkstain/fns.clj
@@ -1,7 +1,8 @@
 (ns inkstain.fns
-  (:require [inkstain.config :as config]
+  (:require [io.github.humbleui.app :as app]
+            [inkstain.config :as config]
             [inkstain.state :as state]))
 
 
 
-(defn maybe-quit [_] (if config/debug? #(reset! state/*window (promise)) #(System/exit 0)))
+(defn maybe-quit [_] (if config/debug? (reset! state/*window (promise)) (app/terminate)))
