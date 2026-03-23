fa79d60d6196d843af2a5a5b65e4b7b815862028
Folcon <folcon@gmail.com>
2026-03-23 12:06:32 +0000

    Add support for saves

---
+++ src/inkstain/persistence.clj
@@ -0,0 +1,34 @@
+(ns inkstain.persistence
+  (:require [clojure.java.io :as io]
+            [clojure.edn :as edn]))
+
+
+
+(def default-comp
+  {:player {:chassis :medium :drive-train :standard}
+   :allies [{:chassis :light  :drive-train :dasher   :enabled? true}
+            {:chassis :light  :drive-train :standard :enabled? true}
+            {:chassis :heavy  :drive-train :standard :enabled? true}
+            {:chassis :medium :drive-train :charger  :enabled? true}
+            {:chassis :medium :drive-train :standard :enabled? true}]})
+
+(defn save-file []
+  (io/file (System/getProperty "user.home") ".inkstain" "mechstain.edn"))
+
+(defn load-data []
+  (let [f (save-file)]
+    (if (.exists f)
+      (edn/read-string (slurp f))
+      {:last-used "default"
+       :presets {"default" default-comp}})))
+
+(defn save-data! [data]
+  (let [f (save-file)]
+    (.mkdirs (.getParentFile f))
+    (spit f (pr-str data))))
+
+(defn save-preset! [preset-name comp]
+  (let [data (load-data)]
+    (save-data! (-> data
+                  (assoc-in [:presets preset-name] comp)
+                  (assoc :last-used preset-name)))))
